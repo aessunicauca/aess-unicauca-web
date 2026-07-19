6d1a205c24a9b1f701291e316048d81d56230710
AESS Unicauca <aess@unicauca.edu.co>
2026-07-19 01:12:22 -0500

    Agregar CSS, JS e imágenes

---
+++ timeline.js
@@ -0,0 +1,35 @@
+
+
+
+// ====================================
+// MENÚ ACTIVO (déjalo si ya lo usabas)
+// ====================================
+
+const sections = document.querySelectorAll("section[id]");
+const navLinks = document.querySelectorAll(".menu a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+
+    });
+
+});
